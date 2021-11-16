194770b7f92b7e81369972426c8bfc1e8056a451
arguxx <argianradityaputra21@gmail.com>
2021-11-16 13:58:22 +0700

    update login page

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,26 +1,38 @@
-@extends('layout.main')
-
-@section('container')
-
-
-<div class="card text-center mx-auto mt-5" style="width: 392px; border: 3px solid #333333;
-box-sizing: border-box;
-box-shadow: 0px 0px 20px 1px rgba(0, 0, 0, 0.25);
-border-radius: 29px; height:554px;">
-    <div class="card-body mt-5">
-      <img src="logo/logo.png" class="mb-5 mt-3" style="width: 150px" alt="">
-
-<form class="my-auto">
-  <div class="mb-3">
-    <input type="text" class="form-control" id="# " placeholder="Masukan E-Mail">
-  </div>
-  <div class="mb-3">
-    <input type="password" class="form-control" id="#" placeholder="Masukan Password">
-  </div>
-<a href="#" style="color: black"><i>Forgot Password</i></a>
-  <button type="submit" class="btn mt-3" style="width:257px;background: linear-gradient(90deg, #AF5AF2 20.35%, rgba(162, 72, 233, 0) 108.56%);
-  border-radius: 15px;">Submit</button>
-</form>
-    </div>
-  </div>
-@endsection
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>
+        Ladeka
+        </title>
+    </head>
+    <body>
+        <div class="container">
+          <div class="card text-center mx-auto mt-5" style="width: 392px; border: 3px solid #333333;
+          box-sizing: border-box;
+          box-shadow: 0px 0px 20px 1px rgba(0, 0, 0, 0.25);
+          border-radius: 29px; height:554px;">
+              <div class="card-body mt-5">
+                <img src="logo/logo.png" class="mb-5 mt-3" style="width: 150px" alt="">
+          
+          <form class="my-auto">
+            <div class="mb-3">
+              <input type="text" class="form-control" id="# " placeholder="Masukan E-Mail">
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control" id="#" placeholder="Masukan Password">
+            </div>
+          <a href="#" style="color: black"><i>Forgot Password</i></a>
+            <button type="submit" class="btn mt-3" style="width:257px;background: linear-gradient(90deg, #AF5AF2 20.35%, rgba(162, 72, 233, 0) 108.56%);
+            border-radius: 15px;">Submit</button>
+          </form>
+              </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
