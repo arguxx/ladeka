e5f8b984273827c14793146e234361796fb3c799
arguxx <argianradityaputra21@gmail.com>
2021-12-27 19:42:32 +0700

    Dropping old Files

--- resources/views/dashboard/dash.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('container')
-    <div style="margin-top: 160px;" class="container mx-5 text-center">
-        <a href="/paket"><i style="margin-right: 100px; color: black;" class="fas fa-car fa-10x"></i></a>
-        <a href="/list-member"><i style="margin-right: 100px; color: black;" class="fas fa-address-card fa-10x"></i></a>
-        <a href="/tambah-member"><i class="fas fa-user-plus fa-10x" style="color: black;"></i></a>
-    </div>
-@endsection
